c91da0e0ee3e67d212cc79622ef2180c7b1175b6
richar plazas <123richarplazas@gmail.com>
2026-06-16 16:28:35 -0500

    Ajuste responsive para móviles y menú hamburguesa

--- script.js
+++ script.js
@@ -295,3 +295,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+const toggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+toggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
